f11cf21fecf8f1ff8cc4cbe4bc033c45337a63f1
ryubyeongsun <abcfg89@naver.com>
2025-11-27 15:07:36 +0900

    first commit

---
+++ src/test/java/com/dietcoach/project/DietCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietcoach.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
